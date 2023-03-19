4f841eb76688918bdb542edb353b6bf83e9dfc1e
haseeb0089 <120918012+haseeb0089@users.noreply.github.com>
2023-03-19 21:09:13 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          
+          <a href="https://www.linkedin.com/in/muhammad-haseeb-sharif-910a841a0/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/HaseebSharif" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
